f8bb6018fd1d6f971b1a7447c8a0ca4e126a98ab
Abraham Fong <abisfong@ucsc.edu>
2021-11-12 00:41:10 -0800

    Set up parts of UsersController actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+  before_action :require_logged_in, only: [:show, :index]
+  
+  def create
+    @user = User.new(strong_params)
+
+    if @user.save
+      login(@user)
+      redirect_to users_url
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def strong_params
+    params.require(:user).permit(:username, :password)
+  end
 end
