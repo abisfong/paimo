4c1f00fd4e221567888c382dd1ecb406dec3cf51
Abraham Fong <abisfong@ucsc.edu>
2021-11-12 00:44:33 -0800

    Delete copy-pasta associations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,6 @@ class User < ApplicationRecord
 
   attr_reader :password
 
-  has_many :goals
-
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return user if user && user.is_password?(password)
