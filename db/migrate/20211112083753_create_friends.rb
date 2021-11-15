7f2fac753c58155c73225b18826c37dbf1cacdfc
Abraham Fong <abisfong@ucsc.edu>
2021-11-14 21:46:05 -0800

    Create Friends migration

--- db/migrate/20211112083753_create_friends.rb
+++ db/migrate/20211112083753_create_friends.rb
@@ -1,8 +1,11 @@
 class CreateFriends < ActiveRecord::Migration[5.2]
   def change
     create_table :friends do |t|
-
+      t.bigint user1_id, null: false
+      t.bigint user2_id, null: false
       t.timestamps
     end
+
+    add_index :friends, [:user1_id, :user2_id], unique: true
   end
 end
