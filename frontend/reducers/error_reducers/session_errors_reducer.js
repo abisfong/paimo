d00e3e1ca5c0c4d5ffb8f5b9d2e5d5372b43556a
Abraham Fong <abisfong@ucsc.edu>
2021-11-19 17:00:57 -0800

    Add error handling in session errors reducer

--- frontend/reducers/error_reducers/session_errors_reducer.js
+++ frontend/reducers/error_reducers/session_errors_reducer.js
@@ -1,6 +1,7 @@
 import { 
   RECEIVE_CURRENT_USER,
-  LOGOUT_CURRENT_USER
+  LOGOUT_CURRENT_USER,
+  RECEIVE_SESSION_ERRORS
 } from "../../actions/session_actions";
 
 const sessionErrorsReducer = (state={}, action) => {
@@ -10,6 +11,8 @@ const sessionErrorsReducer = (state={}, action) => {
     case LOGOUT_CURRENT_USER:
     case RECEIVE_CURRENT_USER:
       return [];
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors.responseJSON;
     default:
       return state;
   }
