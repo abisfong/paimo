f15445ce145785e1b83da30b9a43b4b571f906e4
Abraham Fong <abisfong@ucsc.edu>
2021-11-19 16:17:28 -0800

    Update user errors reducer to empty on RECEIVE_USER

--- frontend/reducers/error_reducers/user_errors.js
+++ frontend/reducers/error_reducers/user_errors.js
@@ -4,6 +4,8 @@ const userErrorsReducer = (state={}, action) => {
   Object.freeze(state);
   const nextState = Object.assign({}, state);
   switch(action.type) {
+    case RECEIVE_USER:
+      return {}
     case RECEIVE_USER_ERRORS:
       return action.errors.responseJSON;
     default:
