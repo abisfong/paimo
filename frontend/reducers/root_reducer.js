fc9c17451eed78ea922b8d43f97298068381cb97
Abraham Fong <abisfong@ucsc.edu>
2021-11-21 16:59:38 -0800

    Create session form files

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,13 +2,15 @@ import { combineReducers } from "redux";
 import usersReducer from "./users_reducer";
 import sessionReducer from "./session_reducer";
 import userErrorsReducer from "./error_reducers/user_errors_reducer";
+import sessionErrorsReducer from "./error_reducers/session_errors_reducer";
 
 const rootReducer = combineReducers({
   entities: combineReducers({
     users: usersReducer
   }),
   errors: combineReducers({
-    user: userErrorsReducer
+    user: userErrorsReducer,
+    session: sessionErrorsReducer
   }),
   session: sessionReducer
 });
