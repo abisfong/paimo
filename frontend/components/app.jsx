f3df20b4cc464e9c742acaddfb23ca80eaaf4298
Abraham Fong <abisfong@ucsc.edu>
2021-11-22 15:15:02 -0800

    Setup sass files for navbar styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -7,15 +7,17 @@ import {
   AuthRoute, 
   ProtectedRoute 
 } from '../util/route';
+import NavContainer from './navbar/nav_container';
 import LoginFormContainer from './auth/login_form_container';
 import SignupFormContainer from './auth/signup_form_container';
 
 const App = () => (
   <>
     <div>Welcome to Paymo!</div>
-    <Link to='/signup'>Signup</Link>
-    <Link to='/login'>Login</Link>
+    {/* <Link to='/signup'>Signup</Link>
+    <Link to='/login'>Login</Link> */}
     {/* <Link to='/logout'></Link> */}
+    <NavContainer></NavContainer>
     <AuthRoute path='/signup' component={SignupFormContainer}></AuthRoute>
     <AuthRoute path='/login' component={LoginFormContainer}></AuthRoute>
   </>
