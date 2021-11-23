1dda61ce5a696b074f50a737a193439bf4b33a82
Abraham Fong <abisfong@ucsc.edu>
2021-11-23 01:31:47 -0800

    Style sigin navbar too look similar to Venmo

--- frontend/components/navbar/index.jsx
+++ frontend/components/navbar/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-import { AuthRoute } from '../util/route';
+import { AuthRoute } from '../../util/route';
 import SplashNavbar from './splash_navbar';
 import SigninNavbar from './signin_navbar';
 
 const Navbar = () => (
   <>
     <Switch>
-      <AuthRoute path='/' component={SplashNavbar} />
       <AuthRoute path='/sign-in' component={SigninNavbar}/>
+      <AuthRoute path='/' component={SplashNavbar} />
     </Switch>
   </>
 );
