6f98325b1c92b2e3cb67d60d66db026b36a70944
Abraham Fong <abisfong@ucsc.edu>
2021-11-22 17:40:07 -0800

    Change Paymo to Paimo

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './frontend/paymo.jsx',
+  entry: './frontend/paimo.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: './bundle.js'
