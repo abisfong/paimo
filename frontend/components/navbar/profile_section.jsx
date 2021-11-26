19eb3bf7b0c24fea1d73044d220cb0cfd8d929e9
Abraham Fong <abisfong@ucsc.edu>
2021-11-26 15:25:25 -0800

    Create ProfileSection component skeleton for sidebar nav

--- frontend/components/navbar/profile_section.jsx
+++ frontend/components/navbar/profile_section.jsx
@@ -1,1 +1,11 @@
 import React from 'react';
+
+export default class ProfileSection extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return "";
+  }
+}
