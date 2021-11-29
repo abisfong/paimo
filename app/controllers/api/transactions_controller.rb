5d046f81fbc3064c3e125b45a192e1fbc7751217
Abraham Fong <abisfong@ucsc.edu>
2021-11-29 15:38:08 -0800

    Secure against user creating transaction not involving current user

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -3,14 +3,17 @@ class Api::TransactionsController < ApplicationController
     @transaction = Transaction.new(transaction_params)
     amount = @transaction.amount
     t_name = transactionee_params[:name]
+    t_id = transactionee_params[:id]
 
     @transaction.amount *= 100
 
     if @transaction.save
       if current_user.id === @transaction.payer_id
         render json: ["You paid your friend: #{t_name} $#{amount}"], status: 200
-      else
+      elsif current_user.id === t_id
         render json: ["You've asked your friend: #{t_name} to make a payment"], status: 200
+      else
+        render json: ["Something went wrong"], status: 400
       end
     else
       render json: @transaction.errors.full_messages, status: 400
