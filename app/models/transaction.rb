592140b63be5a26ebfb280251e082a4e6ab96d4c
Abraham Fong <abisfong@ucsc.edu>
2021-11-29 13:45:34 -0800

    Create associations in Transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,12 @@ class Transaction < ApplicationRecord
   validates :payer_id, :payee_id, :amount, :body, :privacy, presence: true
   validates :complete, inclusion: { in: [true, false] }
   validate :amount, numericality: { greater_than: 0 }
+
+  belongs_to :payer,
+    foreign_key: :payer_id,
+    class: :User
+  
+  belongs_to :payee,
+    foreign_key: :payee_id,
+    class: :User
 end
