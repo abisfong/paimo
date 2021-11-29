c04a9e6e2ffde53b03d03a1e2ccd22f816f616ea
Abraham Fong <abisfong@ucsc.edu>
2021-11-29 14:48:20 -0800

    Create Transaction#create action

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -1,9 +1,23 @@
 class Api::TransactionsController < ApplicationController
   def create
     @transaction = Transaction.new(transaction_params)
-    if @transaction
-      
+    amount = @transaction.amount
+    t_name = transactionee_params[:name]
+
+    @transaction.amount *= 100
+
+    if @transaction.save
+      if current_user.id === @transaction.payer_id
+        render 
+          json: ["You paid your friend: #{t_name} $#{amount}"], 
+          status: 200
+      else
+        render 
+          json: ["You've asked your friend: #{t_name} to make a payment"], 
+          status: 200
+      end
     else
+      render: ["Something went wrong"], status: 400
     end
   end
 
@@ -19,6 +33,12 @@ class Api::TransactionsController < ApplicationController
       :sticker,
       :privacy,
       :complete
+    )
+  end
+
+  def transactionee_params
+    params.require(:transactionee).permit(
+      :name
     )
   end
 end
