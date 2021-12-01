2dce1e65fc81c6e9e062985b5174c0c61b7c8111
Abraham Fong <abisfong@ucsc.edu>
2021-12-01 02:25:16 -0800

    Add note Input to TransactionInputs

--- frontend/components/inputs/transaction_inputs.jsx
+++ frontend/components/inputs/transaction_inputs.jsx
@@ -14,6 +14,11 @@ export default function TransactionInputs(props) {
         type='text'
         onChange={props.update('to')}
       />
+      <Input
+        id='note'
+        type='text'
+        onChange={props.update('note')}
+      />
       <div className='transaction form-submit'>
         <button>Pay</button>
         <button>Request</button>
