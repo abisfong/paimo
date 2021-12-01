5d81a8d814f1d4be4dee3d289505e69eb25b1f70
Abraham Fong <abisfong@ucsc.edu>
2021-12-01 12:43:55 -0800

    Create transaction actions

---
+++ frontend/actions/transaction_actions.js
@@ -0,0 +1,26 @@
+import * as transactionApi from '../util/api/transaction_api';
+
+export const RECEIVE_TRANSACTIONS = 'RECIEVE_TRANSACTIONS';
+export const RECEIVE_TRANSACTION = 'RECEIVE_TRANSACTION';
+export const REMOVE_TRANSACTION = 'REMOVE_TRANSACTION';
+export const RECEIVE_TRANSACTION_ERRORS = 'RECEIVE_TRANSACTION_ERRORS';
+
+export const receiveTransactions = transactions => ({
+  type: RECEIVE_TRANSACTIONS,
+  transactions
+});
+
+export const receiveTransaction = transaction => ({
+  type: RECEIVE_TRANSACTIONS,
+  transaction
+});
+
+export const removeTransaction = transaction => ({
+  type: REMOVE_TRANSACTION,
+  transaction
+});
+
+export const receiveTransactionErrors = errors => ({
+  type: RECEIVE_TRANSACTION_ERRORS,
+  errors
+})
