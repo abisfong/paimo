485e7186f3b2d29847080df354c7994631b09f23
Abraham Fong <abisfong@ucsc.edu>
2021-12-02 15:14:44 -0800

    Create search reducer

---
+++ frontend/reducers/search_reducer.js
@@ -0,0 +1,13 @@
+import { 
+  RECEIVE_SEARCH_RESULTS
+} from "../actions/search_actions";
+
+export default function authReducer(state = {}, action) {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_SEARCH_RESULTS:
+      return action.users
+    default:
+      return state;
+  }
+}
