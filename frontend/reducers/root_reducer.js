08c4bba8ce04b5936580f57c9beefac9b8f603c1
Abraham Fong <abisfong@ucsc.edu>
2021-12-02 15:19:20 -0800

    Attach search reducer to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import entitiesReducer from './entities';
 import errorsReducer from './error_reducers';
 import authReducer from "./auth_reducer";
+import searchReducer from "./search_reducer";
 
 const rootReducer = combineReducers({
   entities: entitiesReducer,
   errors: errorsReducer,
-  auth: authReducer
+  auth: authReducer,
+  search: searchReducer
 });
 
 export default rootReducer;
