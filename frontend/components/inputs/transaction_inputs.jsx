9bbf976070fedab48e2f8cb8cbebbf31b7ce75f2
Abraham Fong <abisfong@ucsc.edu>
2021-12-01 23:19:40 -0800

    Update TransactionInputs imports

--- frontend/components/inputs/transaction_inputs.jsx
+++ frontend/components/inputs/transaction_inputs.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { updateUser } from '../../util/api/user_api';
 import Input from './input';
 
 export default class TransactionInputs extends React.Component {
