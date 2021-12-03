b0b1d07129a327081709edc117b70330bbc55c89
Abraham Fong <abisfong@ucsc.edu>
2021-12-03 00:52:57 -0800

    Add transaction latest reducer to root

--- frontend/reducers/transaction_reducer/index.js
+++ frontend/reducers/transaction_reducer/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import transactionLatestReducer from './transaction_latest_reducer';
 import transactionTypeReducer from './transaction_type_reducer';
 
 export default combineReducers({
-  type: transactionTypeReducer
+  type: transactionTypeReducer,
+  latest: transactionLatestReducer
 });
