95af5f8926a041007dc0729f7e22895d706c2dda
Abraham Fong <abisfong@ucsc.edu>
2021-12-03 09:39:53 -0800

    Finish styling transaction form note

--- frontend/components/inputs/transaction_inputs.jsx
+++ frontend/components/inputs/transaction_inputs.jsx
@@ -64,8 +64,17 @@ class TransactionInputs extends React.Component {
         <Input
           id='note'
           type='text'
+          label='Note'
           className='note'
           onChange={update(['transaction', 'note'])}
+          onFocus={e => {
+            const inputContainer = e.target.parentElement;
+            inputContainer.classList.add('note-focus');
+          }}
+          onBlur={e => {
+            const inputContainer = e.target.parentElement;
+            inputContainer.classList.remove('note-focus');
+          }}
         />
         <div className='form-submit'>
           <button onClick={ () => setTransactionType('payment') }>
