95d31712dfb1a783075961685d46d25ddb58a9d4
Abraham Fong <abisfong@ucsc.edu>
2021-12-04 21:00:56 -0800

    Seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create([
-  { first_name: "Roro", last_name: "J", username: "roj", email: "roj@gmail.com" , password: "password"},
-  { first_name: "Hash", last_name: "F", username: "hashf", email: "hash@gmail.com", password: "password"}
+  { first_name: "Demo", last_name: "Omed", username: "demo", email: "demo@paimo.com" , password: "password"},
+  { first_name: "Roro", last_name: "Staffordshire", username: "roj", email: "roro@paimo.com" , password: "password"},
+  { first_name: "Hashy", last_name: "Pembroke", username: "hashy", email: "hashy@paimo.com", password: "password"},
+  { first_name: "Mike", last_name: "Badson", username: "magicmike", email: "mike@paimo.com", password: "password"},
+  { first_name: "Paulo", last_name: "Blackmouth", username: "papipaulo", email: "paulo@paimo.com", password: "password"},
+  { first_name: "Jack", last_name: "Eggs", username: "eggyboi", email: "jack@paimo.com", password: "password"},
+  { first_name: "Charis", last_name: "Chairs", username: "charts", email: "charis@paimo.com", password: "password"},
+  { first_name: "Chris", last_name: "From the Water", username: "agua", email: "chris@paimo.com", password: "password"},
+  { first_name: "Sam", last_name: "Sing", username: "samsung", email: "sam@paimo.com", password: "password"},
+  { first_name: "Ayce", last_name: "Hardware", username: "ace", email: "ayce@paimo.com", password: "password"},
+  { first_name: "Serena", last_name: "Fong", username: "bbserena", email: "serena@paimo.com", password: "password"},
+  { first_name: "Jamie", last_name: "Tadamaru", username: "jaythebae", email: "jamie@paimo.com", password: "password"},
+  { first_name: "Nicole", last_name: "An", username: "lilnikky", email: "nicole@paimo.com", password: "password"},
+  { first_name: "Abraham", last_name: "Lincoln", username: "abethebabe", email: "abraham@paimo.com", password: "password"},
+  { first_name: "Kenny", last_name: "Ku", username: "cutekenny", email: "kenny@paimo.com", password: "password"},
+  { first_name: "Emily", last_name: "Johnson", username: "ejay", email: "emily@paimo.com", password: "password"},
+  { first_name: "Veronika", last_name: "Madsen", username: "vmad", email: "veronika@paimo.com", password: "password"},
 ]);
