3beda703a20f026b6e44c29ab81e99d88f0480cb
Abraham Fong <abisfong@ucsc.edu>
2021-12-04 20:13:22 -0800

    Revert transaction reducer to old name

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,14 +3,14 @@ import entitiesReducer from './entities';
 import errorsReducer from './error_reducers';
 import authReducer from "./auth_reducer";
 import searchReducer from "./search_reducers";
-import transactionsReducer from "./transactions_reducer";
+import transactionReducer from "./transaction_reducer";
 
 const rootReducer = combineReducers({
   entities: entitiesReducer,
   errors: errorsReducer,
   auth: authReducer,
   search: searchReducer,
-  transaction: transactionsReducer
+  transaction: transactionReducer
 });
 
 export default rootReducer;
