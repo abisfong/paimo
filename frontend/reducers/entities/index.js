9312c7799a5276f0a2b4b0fafa028511c6cc020d
Abraham Fong <abisfong@ucsc.edu>
2021-12-04 20:16:29 -0800

    Change transactionReducer to transactionsReducer

--- frontend/reducers/entities/index.js
+++ frontend/reducers/entities/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
-import transactionReducer from "./transaction_reducer";
+import transactionsReducer from "./transactions_reducer";
 import usersReducer from "./users_reducer";
 
 export default combineReducers({
   users: usersReducer,
-  transactions: transactionReducer
+  transaction: transactionsReducer
 });
