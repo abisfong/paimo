7641b1c74bb635c20b28735ff6e2e9bc7a8cb404
Abraham Fong <abisfong@ucsc.edu>
2021-12-04 19:53:16 -0800

    Update auth reducer to accept new user jbuilder data shape

--- frontend/reducers/auth_reducer.js
+++ frontend/reducers/auth_reducer.js
@@ -11,7 +11,7 @@ export default function authReducer(state = nullSate, action) {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return { currentUser: Object.values(action.user)[0] }
+      return { currentUser: action.user }
     case REMOVE_CURRENT_USER:
       return nullSate;
     default:
