060e48bdc5760b3b4ede8e3564b4fcb8f4f53503
Abraham Fong <abisfong@ucsc.edu>
2021-12-12 18:56:28 -0800

    Make toast message ordering more dynamic

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import entitiesReducer from './entities';
-import errorsReducer from './error_reducers';
+import errorsReducer from './errors_reducer';
 import authReducer from "./auth_reducer";
 import searchReducer from "./search_reducers";
 import transactionReducer from "./transaction_reducer";
