99b792ad9c188ae8a6e7db08debf25bd3e2a42e0
Abraham Fong <abisfong@ucsc.edu>
2021-12-20 16:49:21 -0800

    Finalize functionality for categorizing transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,8 @@
 class Transaction < ApplicationRecord
-  validates :payer_id, :payee_id, :amount, :note, :type, :privacy, presence: true
+  validates :payer_id, :payee_id, :amount, :note, :category, :privacy, presence: true
   validates :complete, inclusion: { in: [true, false] }
   validates :amount, numericality: { greater_than: 0 }
-  validates :type, inclusion: { in: ['request', 'payment'] }
+  validates :category, inclusion: { in: ['request', 'payment'] }
 
   belongs_to :payer,
     foreign_key: :payer_id,
