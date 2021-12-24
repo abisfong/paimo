ebc83b3b425bced1ce4f94ba849f07f00acd6883
Abraham Fong <abisfong@ucsc.edu>
2021-12-24 15:25:19 -0800

    Adjust amount input size to input length dependent on character size

--- frontend/components/inputs/transaction_inputs/amount_input.jsx
+++ frontend/components/inputs/transaction_inputs/amount_input.jsx
@@ -8,22 +8,26 @@ export default class AmountInput extends React.Component {
 
   onChangeHandler() {
     return this.props.update(['transaction', 'amount'], inputEl => {
-      const inputLength = inputEl.value.length;
-      if (inputLength === 0) {
+      const input = inputEl.value;
+      const inputLength = input.length;
+      if (inputLength === 0)
         inputEl.style.placeholder = '0';
-      } else if (inputLength <= 9)
-        
-        inputEl.style.width = `${33 * inputLength}px`;
+      if (inputLength <= 1)
+        inputEl.style.width = '33px';
+      else if (inputLength <= 9)
+        inputEl.style.width = `${this.calculateInputWidth(input)}px`;
       else
-        inputEl.value = inputEl.value.substring(0, 9);
+        inputEl.value = input.substring(0, 9);
     })
   }
 
-  calculateInputWidth(value) {
-    digitsNotOne = /[02-9]/g;
-    widthOfDigitsNotOne = value.match(digitsNotOne).length || 0;
-    widthOfOnesAndPeriods = value.match(/[1.]/g).length || 0;
-    return widthOfDigitsNotOne * 33 + widthOfOnesAndPeriods * 18;
+  calculateInputWidth(input) {
+    const digitsNotOne = /[02-9]/g;
+    const numOfDigitsNotOne = (input.match(digitsNotOne) || []).length;
+    const numOfOnesAndPeriods = (input.match(/[1.]/g) || []).length;
+    console.log(numOfDigitsNotOne);
+    console.log(numOfOnesAndPeriods);
+    return numOfDigitsNotOne * 33 + numOfOnesAndPeriods * 23.11;
   }
 
   render() {
