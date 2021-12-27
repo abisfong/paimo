4c8529b35b265e208f968af9b5d07ae5f2dcd959
Abraham Fong <abisfong@ucsc.edu>
2021-12-26 18:32:23 -0800

    Update search bar value with name of selection

--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -5,26 +5,30 @@ import debounce from '../../utils/debounce';
 export default class SearchBar extends React.Component {
   constructor(props) {
     super(props);
+    this.inputElRef = React.createRef();
     this.search = debounce(e => {
       this.props.search(e.target.value);
     }, 400);
   }
 
-  onChangeHandler(e) {
-    this.search(e);
+  componentDidUpdate() {
+    const inputEl = this.inputElRef.current;
+    const input = inputEl.value;
+    const selectionName = this.props.selectionName;
+    if (selectionName && input !== selectionName)
+      inputEl.value = selectionName;
   }
 
   render() {
-    const selectionName = this.props.selectionName;
-    console.log(selectionName)
     return (
       <Input
         id='search-bar'
         type='text'
         label='To'
         className='search-bar'
+        _ref={this.inputElRef}
         onChange={this.search}
-        placeholder={selectionName || 'Name or username'}
+        placeholder={'Name or username'}
         onFocus={e => {
           const inputContainer = e.target.parentElement;
           inputContainer.classList.add('search-bar-focus');
