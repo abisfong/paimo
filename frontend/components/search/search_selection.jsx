da67594c5e0039adf6a944990f79db7a3806a43b
Abraham Fong <abisfong@ucsc.edu>
2021-12-30 11:26:06 -0800

    Create SearchSelection component

--- frontend/components/search/search_selection.jsx
+++ frontend/components/search/search_selection.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import CloseIcon from '../icons/close_icon';
 
 export default function SearchSelection(props) {
-  
+  return (
+    <div className='search-selection'>
+      <span className='name'>{props.name}</span>
+      <CloseIcon />
+    </div>
+  )
 }
