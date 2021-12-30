5afa895f90a71770ca9615a6310b046a45c0de1d
Abraham Fong <abisfong@ucsc.edu>
2021-12-30 11:34:55 -0800

    Create an onClick handler to close a search selection

--- frontend/components/search/search_selection.jsx
+++ frontend/components/search/search_selection.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import CloseIcon from '../icons/close_icon';
 
 export default function SearchSelection(props) {
+  const onClickHandler = e => {
+    const selectionEl = e.currentTarget;
+    selectionEl.parentElement.removeChild(selectionEl)
+  }
+  
   return (
-    <div className='search-selection'>
+    <div 
+      className='search-selection'
+      onClick={onClickHandler}
+    >
       <span className='name'>{props.name}</span>
       <CloseIcon />
     </div>
