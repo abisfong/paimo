52050036ab3c84baf512e05b66e94ed4fadbaa3d
Abraham Fong <abisfong@ucsc.edu>
2021-12-30 23:35:08 -0800

    Pass selection ids to backend

--- frontend/utils/api/search_api.js
+++ frontend/utils/api/search_api.js
@@ -1,6 +1,6 @@
 export const search = (input, selectionIds) => (
   $.ajax({
     method: 'GET',
-    url: `/api/users?name=${encodeURIComponent(input)}?selectionIds=${selectionIds.join(',')}`
+    url: `/api/users?name=${encodeURIComponent(input)}&selection_ids=${selectionIds.join(',')}`
   })
 );
