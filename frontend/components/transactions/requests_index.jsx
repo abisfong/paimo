effa9891d9db325c2d88384c1c570199ab78970b
Abraham Fong <abisfong@ucsc.edu>
2022-01-04 11:35:19 -0800

    Create a RequestsIndex base component

---
+++ frontend/components/transactions/requests_index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class RequestsIndex extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+      </>
+    )
+  }
+}
