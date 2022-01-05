d15d879dfa05aabca167e541d54ab706f38a0e69
Abraham Fong <abisfong@ucsc.edu>
2022-01-04 16:01:13 -0800

    Create tab actions

---
+++ frontend/actions/tab_actions.js
@@ -0,0 +1,12 @@
+export const RECEIVE_ACTIVITY_TAB_SELECTION = 'RECEIVE_ACTIVITY_TAB_SELECTION';
+export const RECEIVE_REQUESTS_TAB_SELECTION = 'RECEIVE_REQUESTS_TAB_SELECTION';
+
+export const receiveActivityTabSelection = tabNumber => ({
+  type: RECEIVE_ACTIVITY_TAB_SELECTION,
+  tabNumber
+});
+
+export const receiveRequestsTabSelection = tabNumber => ({
+  type: RECEIVE_REQUESTS_TAB_SELECTION,
+  tabNumber
+})
