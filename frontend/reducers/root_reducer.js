ea1707cd05ac6802dda796292aaab69513f4543f
Abraham Fong <abisfong@ucsc.edu>
2022-01-04 16:22:38 -0800

    Connect ui reducer to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,14 +1,16 @@
 import { combineReducers } from "redux";
-import entitiesReducer from './entities';
+import entitiesReducer from './entities_reducer';
 import errorsReducer from './errors_reducer';
 import authReducer from "./auth_reducer";
 import searchReducer from "./search_reducers";
+import uiReducer from './ui_reducer';
 
 const rootReducer = combineReducers({
   entities: entitiesReducer,
   errors: errorsReducer,
   auth: authReducer,
   search: searchReducer,
+  ui: uiReducer
 });
 
 export default rootReducer;
