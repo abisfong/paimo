aa74f8fe0829f462ea193b4f3e235b993005ea01
Abraham Fong <abisfong@ucsc.edu>
2022-01-04 20:44:33 -0800

    Add redirect path to transaction form container

--- frontend/components/forms/transaction_form_container.jsx
+++ frontend/components/forms/transaction_form_container.jsx
@@ -19,6 +19,7 @@ const mapStateToProps = ({ search }) => {
     formHeader: <p className="transaction-title">Paimo | Pay &amp; Request</p>,
     formType: 'Transaction',
     className: 'transaction-form',
+    redirectTo: '/account',
   };
 };
 
