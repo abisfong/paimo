e69cf4cedecc667bcb0e243112052e4c2c98966a
Abraham Fong <abisfong@ucsc.edu>
2022-01-05 23:26:20 -0800

    Create base ProfilePicture component

---
+++ frontend/components/profile_picture.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class ProfilePicture extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+      </>
+    )
+  }
+}
--- frontend/components/transactions/transaction_item.jsx
+++ frontend/components/transactions/transaction_item.jsx
@@ -57,7 +57,7 @@ export default class TransactionItem extends React.Component {
     const timestamp = createTimestamp(new Date(), new Date(transaction.created_at))
     return (
       <div className='transaction'>
-        <img className='profile-picture' alt="Profile picture" />
+        <div className='profile-picture' />
         <div className='content'>
           <header className='header'>
             <span className='message'>
