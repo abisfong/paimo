6792b6ec595cdbfbc1b144680d73e6ab14b7c735
Abraham Fong <abisfong@ucsc.edu>
2022-01-06 21:56:55 -0800

    Remove the use of RequestIndex and only use TransactionIndex

--- frontend/components/transactions/requests_index_container.jsx
+++ frontend/components/transactions/requests_index_container.jsx
@@ -1,29 +1,46 @@
 import React from "react";
 import { connect } from "react-redux";
 import { getTransactions } from '../../actions/transaction_actions';
+import getRequestsSent from "../../utils/components/transaction/get_requests_sent";
+import getRequestsToPay from "../../utils/components/transaction/get_requests_to_pay";
 import RequestsTabsContainer from './requests_tabs_container';
-import RequestsIndex from "./requests_index";
+import TransactionsIndex from "./transactions_index";
 import { 
   deleteTransaction,
   updateTransaction
 } from "../../actions/transaction_actions";
-import getRequestsSent from "../../utils/components/transaction/get_requests_sent";
-import getRequestsToPay from "../../utils/components/transaction/get_requests_to_pay";
 
-const mapStateToProps = ({ entities, auth, ui }) => {
+const mapStateToProps = ({ entities, auth, ui }, ownProps) => {
   const currentTabNumber = ui.tabs.requests;
   const currentUser = auth.currentUser;
+  const firstButtonName = currentTabNumber === 0 ? 'Cancel' : 'Decline';
+  const secondButtonName = currentTabNumber === 0 ? 'Remind' : 'Pay';
   const transactions = entities.transactions;
   const header = (
     <>
-      <h1 className='requests-header'>
-        Incomplete
-      </h1>
+      <h1 className='requests-header'>Incomplete</h1>
       <RequestsTabsContainer/>
+    </>
+  );
+  const actionButtons = id => (
+    <>
+      <button
+        onClick={() => ownProps.deleteTransaction(id)}
+        className='base-action-white-link'
+      >
+        {firstButtonName}
+      </button>
+      <button
+        {...secondButtonName === 'Pay' && { onClick: () => ownProps.updateTransaction(id) }}
+        className='base-action-white-link'
+      >
+        {secondButtonName}
+      </button>
     </>
   )
   
   return {
+    actionButtons,
     currentUser,
     friends: false,
     header,
@@ -43,4 +60,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(RequestsIndex);
+export default connect(mapStateToProps, mapDispatchToProps)(TransactionsIndex);
