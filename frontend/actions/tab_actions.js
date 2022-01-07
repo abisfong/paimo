0683bebb4e4943338bfb19831aedfdcead40d860
Abraham Fong <abisfong@ucsc.edu>
2022-01-06 20:54:50 -0800

    Create actions for user activity tabs

--- frontend/actions/tab_actions.js
+++ frontend/actions/tab_actions.js
@@ -1,5 +1,6 @@
 export const RECEIVE_ACTIVITY_TAB_SELECTION = 'RECEIVE_ACTIVITY_TAB_SELECTION';
 export const RECEIVE_REQUESTS_TAB_SELECTION = 'RECEIVE_REQUESTS_TAB_SELECTION';
+export const RECEIVE_USER_ACTIVITY_TAB_SELECTION = 'RECEIVE_USER_ACTIVITY_TAB_SELECTION';
 
 export const receiveActivityTabSelection = tabNumber => ({
   type: RECEIVE_ACTIVITY_TAB_SELECTION,
@@ -10,3 +11,8 @@ export const receiveRequestsTabSelection = tabNumber => ({
   type: RECEIVE_REQUESTS_TAB_SELECTION,
   tabNumber
 })
+
+export const receiveUserActivityTabSelection = tabNumber => ({
+  type: RECEIVE_ACTIVITY_TAB_SELECTION,
+  tabNumber
+});
