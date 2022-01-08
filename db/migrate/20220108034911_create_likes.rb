3f9563d1e01707e9fde98f0867263a7579326654
Abraham Fong <abisfong@ucsc.edu>
2022-01-07 19:54:27 -0800

    Create Likes table and base controller and model

---
+++ db/migrate/20220108034911_create_likes.rb
@@ -0,0 +1,11 @@
+class CreateLikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :likes do |t|
+      t.bigint :user_id, null: false
+      t.bigint :payment_id, null: false
+      t.timestamps
+    end
+
+    add_index :likes, [:user_id, :payment_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_065111) do
+ActiveRecord::Schema.define(version: 2022_01_08_034911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "payment_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "payment_id"], name: "index_likes_on_user_id_and_payment_id", unique: true
+  end
 
   create_table "transactions", force: :cascade do |t|
     t.bigint "payer_id", null: false
