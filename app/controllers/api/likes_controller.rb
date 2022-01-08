51f641bddd693b602bc5f8a80966b8eccb2b9c1a
Abraham Fong <abisfong@ucsc.edu>
2022-01-07 21:32:49 -0800

    Implement LikesController#create
    Like creations never fail. They always return with a success. But
    they do fail internally because the likes table contraints
    disallow a user from liking a transaction more than once

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,2 +1,13 @@
 class Api::LikesController < ApplicationController
+  def create
+    @like = Like.new(
+      user_id: current_user.id,
+      transaction_id: params[:transaction_id]
+    )
+    @like.save
+    render json: ['Success'], stats: 200
+  end
+
+  def destory
+  end
 end
