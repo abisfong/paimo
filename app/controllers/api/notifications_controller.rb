379af065e131f57e164926019e26dba87d3b71b2
Abraham Fong <abisfong@ucsc.edu>
2022-01-09 18:37:35 -0800

    Add destroy function to NotificationsController

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -7,4 +7,15 @@ class Api::NotificationsController < ApplicationController
       render json: ['Something went wrong'], status: 400
     end
   end
+
+  def destroy
+    @notification = Notification.find(params[:id])
+    
+    if current_user.id === @notification.user_id
+      @notification.destroy
+      render json: ['Success'], status: 200
+    else
+      render json: ['Something went wrong'], status: 400
+    end
+  end
 end
