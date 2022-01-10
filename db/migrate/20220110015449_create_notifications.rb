071f46228ff11175faa63cc285b8a7cdf53d35a9
Abraham Fong <abisfong@ucsc.edu>
2022-01-09 18:13:35 -0800

    Create Notifications table

--- db/migrate/20220110015449_create_notifications.rb
+++ db/migrate/20220110015449_create_notifications.rb
@@ -1,7 +1,10 @@
 class CreateNotifications < ActiveRecord::Migration[5.2]
   def change
     create_table :notifications do |t|
-
+      t.bigint :user_id, null: false
+      t.string :message, null: false
+      t.category :string, null: false
+      t.string :data 
       t.timestamps
     end
   end
