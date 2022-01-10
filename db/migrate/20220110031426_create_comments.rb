6f1b1f55e9090c6525c92c44404221ac52e8e273
Abraham Fong <abisfong@ucsc.edu>
2022-01-09 19:17:35 -0800

    Create Comments migration

--- db/migrate/20220110031426_create_comments.rb
+++ db/migrate/20220110031426_create_comments.rb
@@ -1,8 +1,13 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-
+      t.bigint :user_id, null: false
+      t.bigint :transaction_id, null: false
+      t.string :body, null: false
       t.timestamps
     end
+
+    add_index :comments, :user_id
+    add_index :comments, :transaction_id
   end
 end
