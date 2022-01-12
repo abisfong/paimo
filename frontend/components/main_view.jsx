a0c869cbd15f25127bf014d35d77433e904146d5
Abraham Fong <abisfong@ucsc.edu>
2022-01-12 12:56:24 -0800

    Filter "friends" tab for complete transactions

--- frontend/components/main_view.jsx
+++ frontend/components/main_view.jsx
@@ -20,7 +20,9 @@ export default function MainView(props) {
             {...props} 
             firstTabContent='Friends'
             secondTabContent='You'
-            firstFilter={transactions => transactions}
+            firstFilter={transactions => transactions.filter(transaction => {
+              return transaction.complete
+            })}
             secondFilter={(transactions, {currentUserId}) => 
               getUserTransactions(transactions, currentUserId)
             }
